65df4c578d704a9992969d21de17ff8d393dff34
Shi  Yeqi <shiyeqi@ug241.eecg.toronto.edu>
2017-11-25 16:22:29 -0500

    Adding Phong shading

--- raytracer/light_source.cpp
+++ raytracer/light_source.cpp
@@ -8,6 +8,8 @@
 
 ***********************************************************/
 
+#include <algorithm>
+#include <cassert>
 #include <cmath>
 #include "light_source.h"
 
@@ -20,5 +22,30 @@ void PointLight::shade( Ray3D& ray ) {
 	// is available.  So be sure that traverseScene() is called on the ray 
 	// before this function.  
 
+	// point of intersection
+	auto point = ray.intersection.point;
+	// point material
+	auto material = *(ray.intersection.mat);
+	// light direction
+	auto lightDir = (_pos - point);
+	lightDir.normalize();
+	// normal at point
+	auto normal = ray.intersection.normal;
+	// diffuse component. FIXME
+	auto diffuseLight = std::max(normal.dot(lightDir), 0.0);
+	auto diffuseComponent =  material. diffuse * Colour(diffuseLight, diffuseLight, diffuseLight);
+	// specular direction
+	auto specularDir = (2.0 * lightDir.dot(normal)*normal - lightDir);
+	specularDir.normalize();
+	// view dir, is the opposite of ray dir
+	auto viewDir = -ray.dir;
+	// assert(viewDir.length() == 1);
+	// specular component
+	auto specularLight = std::pow(std::max(specularDir.dot(viewDir), 0.0), material.specular_exp);
+	auto specularComponent = material.specular * Colour(specularLight, specularLight, specularLight);
+	// final color, should be added to existing color of the ray
+	auto finalColor = (material.ambient + diffuseComponent + specularComponent);
+	finalColor.clamp();
+	ray.col = ray.col + finalColor;
 }
 
