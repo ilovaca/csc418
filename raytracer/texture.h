f8c7d46f0c6d3b769b5387cb2bae6512d3fc30a9
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2017-11-26 17:40:18 -0500

    Added the texture subroutines.

---
+++ raytracer/texture.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   texture.h
+ * Author: armageddonknight
+ *
+ * Created on November 26, 2017, 5:33 PM
+ */
+
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+void texture_init();
+
+#endif /* TEXTURE_H */
+
